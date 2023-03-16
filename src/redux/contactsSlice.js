0198e4bcae331d19d39a1ff739ac4bee17a2eb00
yarqui <y.pelykh@gmail.com>
2023-03-16 18:49:45 +0200

    refactors contactsSlice: builder & addMatcher

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -7,6 +7,14 @@ const contactsInitialState = {
   error: null,
 };
 
+const isPendingAction = action => {
+  return action.type.endsWith('pending');
+};
+
+const isRejectedAction = action => {
+  return action.type.endsWith('rejected');
+};
+
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -19,29 +27,52 @@ const handleRejected = (state, action) => {
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsInitialState,
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [addContact.pending]: handlePending,
-    [deleteContact.pending]: handlePending,
-    [fetchContacts.rejected]: handleRejected,
-    [addContact.rejected]: handleRejected,
-    [deleteContact.rejected]: handleRejected,
-    [fetchContacts.fulfilled](state, { payload }) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = payload;
-    },
-    [addContact.fulfilled](state, { payload }) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(payload);
-    },
-    [deleteContact.fulfilled](state, { payload }) {
-      return {
-        isLoading: false,
-        error: null,
-        items: state.items.filter(item => item.id !== payload.id),
-      };
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = payload;
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items.push(payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        return {
+          isLoading: false,
+          error: null,
+          items: state.items.filter(item => item.id !== payload.id),
+        };
+      })
+      .addMatcher(isPendingAction, handlePending)
+      .addMatcher(isRejectedAction, handleRejected);
   },
 });
+// extraReducers: {
+//   [fetchContacts.pending]: handlePending,
+//   [addContact.pending]: handlePending,
+//   [deleteContact.pending]: handlePending,
+//   [fetchContacts.rejected]: handleRejected,
+//   [addContact.rejected]: handleRejected,
+//   [deleteContact.rejected]: handleRejected,
+//   [fetchContacts.fulfilled](state, { payload }) {
+//     state.isLoading = false;
+//     state.error = null;
+//     state.items = payload;
+//   },
+//   [addContact.fulfilled](state, { payload }) {
+//     state.isLoading = false;
+//     state.error = null;
+//     state.items.push(payload);
+//   },
+//   [deleteContact.fulfilled](state, { payload }) {
+//     return {
+//       isLoading: false,
+//       error: null,
+//       items: state.items.filter(item => item.id !== payload.id),
+//     };
+//   },
+// },
+// });
